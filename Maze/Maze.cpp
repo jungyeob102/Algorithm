b0c52606fabf4ed173acf3f257dad1e22425f7bf
jungyeob102 <asdf95102@naver.com>
2022-01-31 00:56:00 +0900

    맵만들기

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -1,9 +1,19 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "ConsoleHelper.h"
+#include "Board.h"
+#include "Player.h"
+
+Board board;
+Player player;
 
 int main()
 {
+	::srand(static_cast<unsigned>(time(nullptr)));
+
+	board.Init(25, &player);
+	player.Init(&board);
+
 	uint64 lastTick = 0;
 	while (true)
 	{
@@ -16,19 +26,9 @@ int main()
 		//입력(Input)
 
 		//로직(Logic)
+		player.Update(deltaTick);
 
 		//렌더링(Rendering)
-		ConsoleHelper::SetCursorPosition(0, 0);
-		ConsoleHelper::ShowConsoleCursor(false);
-		ConsoleHelper::SetCursorColor(ConsoleColor::RED);
-
-		const char* TILE = "■";
-		for (int32 y = 0; y < 25; y++)
-		{
-			for (int32 x = 0; x < 25; x++)
-				cout << TILE;
-			cout << endl;
-		}
-		
+		board.Render();
 	}
 }
--- Maze/pch.h
+++ Maze/pch.h
@@ -5,3 +5,51 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+struct Pos
+{
+	bool operator==(Pos& other)
+	{
+		return y == other.y && x == other.x;
+	}
+
+	bool operator!=(Pos& other)
+	{
+		return !(*this == other);
+	}
+
+	Pos operator+(Pos& other)
+	{
+		Pos ret;
+		ret.y = y + other.y;
+		ret.x = x + other.x;
+	}
+	
+	Pos operator-(Pos& other)
+	{
+		Pos ret;
+		ret.y = y - other.y;
+		ret.x = x - other.x;
+		return ret;
+	}
+
+	Pos& operator+=(Pos& other)
+	{
+		y += other.y;
+		x += other.x;
+		return *this;
+	}
+
+	int32 y = 0;
+	int32 x = 0;
+};
+
+enum Dir
+{
+	DIR_UP = 0,
+	DIR_LEFT,
+	DIR_DOWN,
+	DIR_RIGHT,
+
+	DIR_COUNT
+};
