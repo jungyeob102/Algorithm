899cf95f15ba7b4cd4c2e32a696bad2aa11f74f9
jungyeob102 <asdf95102@naver.com>
2022-01-30 22:14:07 +0900

    Preferences

---
+++ Maze/Maze.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "ConsoleHelper.h"
+
+int main()
+{
+	uint64 lastTick = 0;
+	while (true)
+	{
+#pragma region 프레임 관리
+		const uint64 currentTick = ::GetTickCount64();
+		const uint64 deltaTick = currentTick - lastTick;
+		lastTick = currentTick;
+#pragma endregion
+
+		//입력(Input)
+
+		//로직(Logic)
+
+		//렌더링(Rendering)
+		ConsoleHelper::SetCursorPosition(0, 0);
+		ConsoleHelper::ShowConsoleCursor(false);
+		ConsoleHelper::SetCursorColor(ConsoleColor::RED);
+
+		const char* TILE = "■";
+		for (int32 y = 0; y < 25; y++)
+		{
+			for (int32 x = 0; x < 25; x++)
+				cout << TILE;
+			cout << endl;
+		}
+		
+	}
+}
