4a52233899360ad05d739ddecfc5892310277b1f
jungyeob102 <asdf95102@naver.com>
2022-02-06 23:02:27 +0900

    자료구조 Queue

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -3,10 +3,68 @@
 #include <list>
 #include <stack>
 #include <string>
+#include <queue>
 using namespace std;
 
+//배열로 구현
 template<typename T>
-class Stack
+class ArrayQueue
+{
+public:
+	ArrayQueue()
+	{
+		//_container.resize(100);
+	}
+
+	void push(const T& value)
+	{
+		if (_size == _container.size())
+		{
+			int newSize = max(1, _size * 2); //max 함수 = 둘 중 더 큰 수를 리턴해준다. ( 0이 나오는 걸 방지하기 위하여)
+			vector<T> newData;
+			newData.resize(newSize);
+
+			for (int i = 0; i < _size; ++i)
+			{
+				int index = (_front + i) % _container.size();
+				newData[i] = _container[index];
+			}
+
+			_container.swap(newData);
+			_front = 0;
+			_back = _size;
+		}
+
+		_container[_back] = value;
+		_back = (_back + 1) % _container.size();
+		_size++; //데이터의 갯수를 의미
+	}
+
+	void pop()
+	{
+		_front = (_front + 1) % _container.size();
+		_size--;
+	}
+
+	T& front()
+	{
+		return _container[_front];
+	}
+
+	bool empty() { return _size == 0; }
+	int size() { return _size; }
+
+private:
+	vector<T> _container;
+
+	int _front = 0;
+	int _back = 0;
+	int _size = 0;
+};
+
+//리스트로 구현
+template<typename T>
+class ListQueue
 {
 public:
 	void push(const T& value)
@@ -16,22 +74,36 @@ public:
 
 	void pop()
 	{
-		_container.pop_back();
+		_container.pop_front();
 	}
 
-	T& top()
+	T& front()
 	{
-		return _container.back();
+		return _container.front();
 	}
 
 	bool empty() { return _container.empty(); }
 	int size() { return _container.size(); }
 
 private:
-	vector<T> _container;
+	list<T> _container;
 };
+
 
 int main()
 {
-	
+	ArrayQueue<int> q;
+
+	for (int i = 0; i < 100; ++i)
+		q.push(i);
+
+	while (q.empty() == false)
+	{
+		int value = q.front();
+		q.pop();
+		cout << value << endl;
+	}
+
+	int size = q.size();
+	cout << size << endl;
 }
